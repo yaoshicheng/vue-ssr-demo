5420711c14fddf82e192da8f2bc43400ed8e2d1c
yaoshicheng <929239151@qq.com>
2019-03-21 13:36:11 +0800

    update

--- router.js
+++ router.js
@@ -2,8 +2,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-import My from "./components/My.vue";
-import Home from "./components/Home.vue";
+import My from "../components/My.vue";
+import Home from "../components/Home.vue";
 
 Vue.use(Router);
 
@@ -11,10 +11,10 @@ export default function createRouter () {
   return new Router({
     mode: 'history',
     routes: [
-      // { path: '/foo', component: () => import('./components/My.vue') },
-      // { path: '/test', component:()=>import("./components/Home.vue")},
+      // { path: '/home', component: () => import ('../components/Home.vue') },
+      { path: '/', component: Home },
+      { path: '/my', component:My},
       { path: '/home', component: Home },
-      { path: '/my', component: My },
     ]
   })
 }
